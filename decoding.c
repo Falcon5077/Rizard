a006b398f23c6db7bb730b400cb2d0e0c9f1f4e7
KIM_JINBUM <jinbum9958@gmail.com>
2021-05-31 06:45:30 +0900

    ITEMS

--- decoding.c
+++ decoding.c
@@ -3,12 +3,15 @@
 #include <string.h>
 #define MAX 255
 char* fName;
-char* str1 = "BOMB";
-char* str2 = "POSTION";
-char* str3 = "CURE";
-char* str4 = "BOOK";
-char* str5 = "SHIELD";
-char* str6 = "CANNON";
+
+char* ITEM_NAME[] = {
+    "BOMB",
+    "POSTION",
+    "CURE",
+    "BOOK",
+    "SHIELD",
+    "CANNON"
+};
 // 파일포인터 선언
 FILE* fp;
 // USER_STATUS 변수들
@@ -17,9 +20,9 @@ char ID[MAX], NAME[MAX];
 char GENDER, age;
 unsigned short Coin;
 // ITEMS 변수들
-unsigned char ITEMS_num, ITEMS_amount;
+unsigned char ITEMS_sort, ITEMS_count, ITEMS_num;
 // FRIENDS LIST
-unsigned char FRIEND_ID_length, FRIEND_Name_length;
+char FRIEND_ID_length, FRIEND_Name_length;
 char gender, age;
 char ID[MAX], NAME[MAX];
 // Description
@@ -27,9 +30,9 @@ char ID[MAX], NAME[MAX];
 // 변수들
 unsigned char tmpLen;
 // 함수들
-char ReadLen(char* target);
+char ReadLen(void);
 char Check(char* len);
-void ITEMS(void);
+void ITEM_fun(void);
 
 // main 함수
 int main(int argc, char* argv[]) {
@@ -41,41 +44,70 @@ int main(int argc, char* argv[]) {
 
     fName = argv[1];
     fp = fopen(fName, "rb");
-
-    ITEMS();
+    ITEM_fun();
     return 0;
 }
 
-void ITEMS(void) {
-    unsigned char sort, num, count;
+void ITEM_fun(void) {
+    ITEMS_sort = ReadLen();
+    ITEMS_count = ReadLen();
+    printf("*ITEMS*\n");
 
-    sort = ReadLen("순서 인지");
-    printf("%d", sort);
-    //// sort가 1이면 순서대로
-    //if (sort == 1) {
-    //    
-    //}
+    // sort가 1이면 순서대로
+    if (ITEMS_sort == 0) {
+        if ((ITEMS_count >= 1) && (ITEMS_count <= 4)) { // ITEMS 갯수가 1이상 4이하일때
+            char ITEMS[6];
+            int T_num = ReadLen(), n = 6;
+            while (n != 0) {
+                ITEMS[--n] = T_num % 2;
+                T_num /= 2;
+            }
+            for (int i = 0; i < 6; i++) {
+                if (ITEMS[i] != 0) {
+                    ITEMS_num = ReadLen();
+                    printf("%s : %d\n", ITEM_NAME[i], ITEMS_num);
+                }
+                else
+                    continue;
+            }
+        }
+        else if ((ITEMS_count >= 5) && (ITEMS_count <= 6)) { // ITEMS 갯수가 5이상 6이하일때
+            unsigned char ITEMS[6];   // 배열의 크기는 6으로 고정
+            for (int i = 0; i < 6; i++) {
+                ITEMS[i] = ReadLen();   // 차례대로 규칙해소
+                if (ITEMS[i] == 0) // ITEMS배열에 저장된 수가 0이면 출력 x
+                    continue;
+                printf("%s : %d\n", ITEM_NAME[i], ITEMS[i]);
+            }
+        }
+    }
 
-    //// sort가 0이면 순서대로x
-    //else if (sort == 0) {
+    // sort가 0이면 순서대로x
+    else if (ITEMS_sort == 1) {
+        char ITEMS[ITEMS_count * 2];    // 배열의 크기를 총 갯수 * 2로 고정
+        for (int i = 0; i < ITEMS_count * 2; i++)
+            ITEMS[i] = ReadLen(); 
 
-    //}
+        for (int j = 0; j < ITEMS_count * 2; j++) {
+            printf("%s : ", ITEM_NAME[ITEMS[j]]);
+            printf("%d\n", ITEMS[++j]);
+        }
+    }
+    printf("\n");
 }
 
-char ReadLen(char* target)	// Check 함수 먼저 읽으3
+char ReadLen()	// Check 함수 먼저 읽으3
 {
     char len[3];
     unsigned char m_len;
 
-    for (int t = 0; t < 3; t++) {
+    for (int t = 0; t < 3; t++)
         fread(&len[t], sizeof(unsigned char), 1, fp);	// ID길이 3개 읽어옴
-    }
 
     m_len = Check(&len[0]);	// 읽어온 ID길이 3개를 Check 함수로 보내서 복원시킴 (667 을 보내면 6이 리턴됨)	
 
     return m_len;
 }
-
 
 char Check(char* tmp)      // tmp 가 KKK 이면
 {
