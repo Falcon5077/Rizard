0eda91745d45db8176e1f977f8de9b2dd0404ccb
KIM_JINBUM <jinbum9958@gmail.com>
2021-05-31 07:17:58 +0900

    경우 추가

--- test.c
+++ test.c
@@ -8,7 +8,49 @@ int main(int argc, char* argv[])
 	unsigned char str;
 
 	char len;
+	unsigned char* arr;
 	FILE* file = fopen(fileName, "wb");
+
+	len = 8;
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+
+	arr = "KKKMMMUUU___CCCSSS222333";
+	fwrite(arr, strlen(arr), sizeof(unsigned char), file);
+
+	len = 14;
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+
+	arr = "HHHOOONNNGGG   CCCHHHAAANNN   PPPAAARRRKKK";
+	fwrite(arr, strlen(arr), sizeof(unsigned char), file);
+
+	len = 'F';
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+
+	len = 33;
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+
+	len = 200;
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+	fwrite(&len, 1, sizeof(char), file);
+
+	len = 5;
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+
+	unsigned short coin = 43210;
+	fwrite(&coin, 1, sizeof(unsigned short), file);
+	fwrite(&coin, 1, sizeof(unsigned short), file);
+	fwrite(&coin, 1, sizeof(unsigned short), file);
 
 	len = 1;
 	fwrite(&len, 1, sizeof(unsigned char), file);
@@ -39,18 +81,16 @@ int main(int argc, char* argv[])
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
-	
-		len = 2;
-		fwrite(&len, 1, sizeof(unsigned char), file);
-		fwrite(&len, 1, sizeof(unsigned char), file);
-		fwrite(&len, 1, sizeof(unsigned char), file);
 
-		len = 3;
-		fwrite(&len, 1, sizeof(unsigned char), file);
-		fwrite(&len, 1, sizeof(unsigned char), file);
-		fwrite(&len, 1, sizeof(unsigned char), file);
+	len = 2;
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
 
-	fclose(file);
+	len = 3;
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
+	fwrite(&len, 1, sizeof(unsigned char), file);
 
 	return 0;
 }
