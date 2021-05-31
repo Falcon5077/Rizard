f1c3105e7ff5457f008e5238c12a9b7eb4fef01c
Kim jin bum <jinbum9958@gmail.com>
2021-05-31 19:17:47 +0900

    Update test.c

--- test.c
+++ test.c
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
 	fwrite(&coin, 1, sizeof(unsigned short), file);
 	fwrite(&coin, 1, sizeof(unsigned short), file);
 
-	len = 1; // sort == 1
+	len = 1; // sort == 0 or 1
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
@@ -62,7 +62,11 @@ int main(int argc, char* argv[])
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
 
-	len = 3; // 아이템유무를 이진수로 나타낸걸 10진수 변환한 값
+	// sort가 1일 경우 여기서 부터 아이템 종류 / 아이템 갯수 입력
+	// sort가 0일 경우 두가지경우로 구분
+	// case 1일때 여기에 아이템 유무를 구분한 이진수를 10진수로 변환한 값 저장
+	// case 2일때 여기서 부터 개수 저장, 0은 무시하고 넘어감
+	len = 3; 
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
 	fwrite(&len, 1, sizeof(unsigned char), file);
