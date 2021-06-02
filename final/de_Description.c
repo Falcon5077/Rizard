e6fa6f535a0b98a876d1f3be43f429b6185485e9
hyeongrae-kim <soboru1002@gmail.com>
2021-06-03 02:06:05 +0900

    Complete_version_decoding_description

--- final/de_Description.c
+++ final/de_Description.c
@@ -31,7 +31,7 @@ unsigned char CheckUnchar(unsigned char* tmp){
 	else{
 		real = tmp[0];
 	}
-	printf("%c", real);
+
 	return real;
 }
 
@@ -40,12 +40,10 @@ unsigned char ReadUnchar(){
 	unsigned char len[3];
 	unsigned char m_unchar;
 
-	for(int i=0; i<3; i++)
+	for(int i=0; i<3; i++){
 		fread(&len[i], sizeof(unsigned char), 1, fp1);
-
+	}
 	m_unchar = CheckUnchar(len);
-
-	printf("%c", m_unchar);
 	
 	return m_unchar;
 }
@@ -55,38 +53,35 @@ void makedes(){
 	while(!feof(fp1)){
 		des[i++] = ReadUnchar();
 	}
+
+	des[i-1] = '\0';
 }
 
 void ArrToTxt(){
 	int i=0; //des배열 반복위한 변수
 	int u=0; //txt배열 반복위한 변수
+
 	int n=0; //반복횟수 저장변수
 	int d=0; //반복문장 가져올 시 처음부터 검사할 변수
 
 	fp2=fopen("decoded_data1.txt", "w");
 
 	fprintf(fp2, "\n*DESCRIPTION*\n");
-	
-	while(des[i] != '\0'){ //배열 끝까지 반복		
+	while(des[i]){ //배열 끝까지 반복		
 		if(des[i]>= 'A' && des[i]<= 'Z'){ //문자가 반복되지않고 나올때
-			txt[u] = des[i];
-			fprintf(fp2, "%c", txt[u]);
-			u++;
-			i++;
+			txt[u] = des[i++];
+			fprintf(fp2, "%c", txt[u++]);
 		}
 		else if(des[i] == '\n'){ //개행문자가 나올때
-			txt[u] = des[i];
-			fprintf(fp2, "%c", txt[u]);
-			u++;
-			i++;
+			txt[u] = des[i++];
+			fprintf(fp2, "%c", txt[u++]);
 		}
 		else if(des[i]>= '0'-10 && des[i]<= '9'-10){ //한자리 또는 세자리 이상숫자일때
-			if(des[i+1]>=0 && des[i+1]<= 28 && des[i+1]!=10){ //세자리 이상 숫자일때
+			if(des[i+1]>=3 && des[i+1]<= 28 && des[i+1]!=10){ //세자리 이상 숫자일때
 				n=des[i+1];
 				for(int k=0; k<n; k++){ //다음칸에 나오는 반복숫자만큼 반복
 					txt[u] = des[i]+10;
-					fprintf(fp2, "%c", txt[u]);
-					u++;
+					fprintf(fp2, "%c", txt[u++]);
 				}
 				i=i+2;
 			}
@@ -94,32 +89,27 @@ void ArrToTxt(){
 				n=10;
 				for(int k=0; k<n; k++){
 					txt[u] = des[i]+10;
-					fprintf(fp2, "%c", txt[u]);
-					u++;
+					fprintf(fp2, "%c", txt[u++]);
 				}
 				i=i+2;
 			}
 			else{ //한자리 숫자일때
-				txt[u] = des[i]+10;
-				fprintf(fp2, "%c", txt[u]);
-				u++;
-				i++;
+				txt[u] = des[i++]+10;
+				fprintf(fp2, "%c", txt[u++]);
 			}
 		}
 		else if(des[i]>= '0'-20 && des[i]<='9'-20){ //두자리 숫자일때
-			txt[u] = des[i]+20;
-			txt[u+1] = des[i]+20;
-			fprintf(fp2, "%c%c", txt[u], txt[u+1]);
+			txt[u++] = des[i]+20;
+			txt[u++] = des[i]+20;
+			fprintf(fp2, "%c%c", txt[u-2], txt[u-1]);
 			i++;
-			u=u+2;
 		}
 		else if(des[i]>='a'&& des[i]<='z'){ //반복되는 문자의 경우
-			if(des[i+1] >= 0 && des[i+1] <= 28 && des[i+1]!=10){ //세번이상 반복되는 문자
+			if(des[i+1] >= 3 && des[i+1] <= 28 && des[i+1]!=10){ //세번이상 반복되는 문자
 					n=des[i+1];
 					for(int k=0; k<n; k++){
 						txt[u] = des[i]-32;
-						fprintf(fp2, "%c", txt[u]);
-						u++;
+						fprintf(fp2, "%c", txt[u++]);
 					}
 					i=i+2;
 			}
@@ -127,18 +117,15 @@ void ArrToTxt(){
 				n=10;
 				for(int k=0; k<n; k++){
 					txt[u] = des[i]-32;
-					fprintf(fp2, "%c", txt[u]);
-					u++;
+					fprintf(fp2, "%c", txt[u++]);
 				}
 				i=i+2;
 			}
 			else{ //두번 반복되는 경우
-				txt[u] = des[i]-32;
-				u++;
-				txt[u] = des[i]-32;
-				fprintf(fp2, "%c%c", txt[u], txt[u]);
+				txt[u++] = des[i]-32;
+				txt[u++] = des[i]-32;
+				fprintf(fp2, "%c%c", txt[u-2], txt[u-1]);
 				i++;
-				u++;
 			}
 		}
 		else if(des[i] == '='){ //문장반복구문 나올경우
@@ -152,10 +139,8 @@ void ArrToTxt(){
 			}
 
 			while(txt[d]!='\n'){
-				txt[u] = txt[d];
-				fprintf(fp2, "%c", txt[u]);
-				u++;
-				d++;
+				txt[u] = txt[d++];
+				fprintf(fp2, "%c", txt[u++]);
 			}
 
 			i=i+2;
@@ -165,8 +150,8 @@ void ArrToTxt(){
 			fprintf(fp2, "%c", des[i]);
 			i++;
 		}
-	}
 
+	}
 	fclose(fp2);
 }
 
@@ -177,9 +162,11 @@ int main(){
 	}
 
 	makedes();
+	ArrToTxt();
+
+	//printf("%s\n", des);
+	//printf("%s\n", txt);
 
 	fclose(fp1);
-
-	ArrToTxt();
 	return 0;
 }
