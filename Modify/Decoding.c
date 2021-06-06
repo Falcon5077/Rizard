8ee1c0f4b464dcbab3fa7a415c51fc0ec3c369ff
gieun <namgigun@gmail.com>
2021-06-06 22:29:31 +0900

    friendlist주석완료

--- Modify/Decoding.c
+++ Modify/Decoding.c
@@ -109,15 +109,15 @@ char* ITEM_NAME[] = {
 
 // 아이템 디코딩 함수
 void Write_ITEMS(void) {
-   ITEMS_sort = ReadLen(); // 파일에 있는 순서를 ITEM_sort 변수에 불러오고, 
+   ITEMS_sort = ReadUnchar(); // 파일에 있는 순서를 ITEM_sort 변수에 불러오고, 
    fprintf(fp2, "*ITEMS*\n");
 
    // ITEM_sort 값이 0이면 본래의 텍스트가 ITEM_NAME[]의 문자열 순서대로 정렬되지 않음을 의미
    if (ITEMS_sort == 0) {
-      ITEMS_count = ReadLen(); // 파일에 있는 아이템 개수를 ITEMS_count에 저장
+      ITEMS_count = ReadUnchar(); // 파일에 있는 아이템 개수를 ITEMS_count에 저장
       unsigned char ITEMS[ITEMS_count * 2];    // 배열의 크기를 ITEMS_count * 2로 선언
       for (int i = 0; i < ITEMS_count * 2; i++)
-         ITEMS[i] = ReadLen(); // ITEMS[]에 차례대로 아이템 종류와 개수를 입력
+         ITEMS[i] = ReadUnchar(); // ITEMS[]에 차례대로 아이템 종류와 개수를 입력
 
       for (int j = 0; j < ITEMS_count * 2; j++) {
          fprintf(fp2, "%s: ", ITEM_NAME[ITEMS[j]]);
@@ -130,14 +130,14 @@ void Write_ITEMS(void) {
       ITEMS_count = ITEMS_sort; // ITEMS_sort가 0이 아닌 경우 바로 총 개수로 인지하고 ITEMS_count에 대입
       if ((ITEMS_count >= 1) && (ITEMS_count <= 4)) { // ITEMS 갯수가 1이상 4이하일때
          char ITEMS[6]; // 10진수 값을 소인수분해해서 ITEMS배열에 저장
-         int T_num = ReadLen(), n = 6; // 파일에서 10진수 값을 받아와서
+         int T_num = ReadUnchar(), n = 6; // 파일에서 10진수 값을 받아와서
          while (n != 0) {
             ITEMS[--n] = T_num % 2; // 10진수 값을 2진수 형태로 소인수 분해
             T_num /= 2;
          }
          for (int i = 0; i < 6; i++) {
             if (ITEMS[i] != 0) { // 2진수 값이 1일 경우
-               ITEMS_num = ReadLen(); // ITEMS_num를 읽어와서
+               ITEMS_num = ReadUnchar(); // ITEMS_num를 읽어와서
                fprintf(fp2, "%s: %d\n", ITEM_NAME[i], ITEMS_num); // 아이템 이름과 개수를 출력
             }
             else
@@ -147,7 +147,7 @@ void Write_ITEMS(void) {
       else if ((ITEMS_count >= 5) && (ITEMS_count <= 6)) { // ITEMS 갯수가 5이상 6이하일때
          unsigned char ITEMS[6];   // 배열의 크기는 6으로 고정
          for (int i = 0; i < 6; i++) { // 저장된 형태는 배열 6개에 아이템별 개수만 저장되어 있으므로
-            ITEMS[i] = ReadLen();   // 차례대로 압축규칙 해소
+            ITEMS[i] = ReadUnchar();   // 차례대로 압축규칙 해소
             if (ITEMS[i] == 0) // ITEMS배열에 저장된 수가 0이면 출력 x
                continue;
             fprintf(fp2, "%s: %d\n", ITEM_NAME[i], ITEMS[i]); // 0이 아니면 출력
@@ -160,58 +160,60 @@ void Write_ITEMS(void) {
 
 // 동맹 변수
 typedef struct {
-	unsigned char I_length;
-	unsigned char N_length;
-	char ID[255];
-	char name[255];
-	char gender;
-	char age;
-	char buffer[1000];
-	char buffer_n[1000];
-	char buffer_g[3];
-	char buffer_age[3];
+	unsigned char I_length; // ID길이
+	unsigned char N_length; // 이름 길이
+	char ID[255]; //ID
+	char name[255]; //이름
+	char gender; // 성별
+	char age; // 나이
+	char buffer[1000]; // ID 버퍼
+	char buffer_n[1000]; // 이름 버퍼
+	char buffer_g[3]; // 성별 버퍼
+	char buffer_age[3]; // 나이 버퍼
 }info; // 동맹 구조체
 
 info FRIEND[100]; //동맹 배열
 unsigned char num; // 동맹수
 
-void Checkstr_I(char* buffer, int F_num) {
-	char tmp[3];
+void Checkstr_I(char* buffer, int F_num) { // ID buffer에 저장된 데이터에서 필요한 정보를 추출해 ID정보를 받아오는 함수
+	char tmp[3]; // 3개의 저장된 정보를 받아오는 배열
 	int index = 0;
 	for (int i = 0; buffer[i] != 0; i = i + 3) {
 		tmp[0] = buffer[i];
 		tmp[1] = buffer[i + 1];
-		tmp[2] = buffer[i + 2];
-		FRIEND[F_num].ID[index] = CheckUnchar(tmp);
+		tmp[2] = buffer[i + 2]; //tmp 안에 저장된 buffer정보를 받은후
+		FRIEND[F_num].ID[index] = CheckUnchar(tmp); // Checkunchar 로 3개중 검증된 하나의 정보를 추출한다.
 		index++;
-	}
-} //ID buffer에 저장된 데이터를 추출한다.
-
-void Checkstr_N(char* buffer, int F_num) {
-	char tmp[3];
-	int index = 0;
-	for (int i = 0; buffer[i] != 0; i = i + 3) {
-		tmp[0] = buffer[i];
-		tmp[1] = buffer[i + 1];
-		tmp[2] = buffer[i + 2];
-		FRIEND[F_num].name[index] = CheckUnchar(tmp);
-		index++;
-	}
-} //name buffer에 저장된 데이터를 추출한다.
-
-void FRIEND_READ() { // 인코더 파일 읽어오는 함수
-	num = ReadUnchar(); // 동맹수를 읽어 온다.
-	int x = 0;
-	for (int i = 0; i < num; i++) {
-		FRIEND[i].I_length = ReadUnchar();
-		fread(FRIEND[i].buffer, sizeof(char), FRIEND[i].I_length * 3, fp1);
-		FRIEND[i].N_length = ReadUnchar();
-		fread(FRIEND[i].buffer_n, sizeof(char), FRIEND[i].N_length * 3, fp1);
-		fread(FRIEND[i].buffer_g, sizeof(char), 3, fp1);
-		fread(FRIEND[i].buffer_age, sizeof(char), 3, fp1);
+		//이 과정을 buffer배열이 끝날때까지 반복해 한글자씩 ID구조체 변수에 받아온다.
 	}
 }
-void FRIEND_SAVE() { // buffer에 있는 정보를 걸러서 출력하고자하는 구조체에 저장
+
+void Checkstr_N(char* buffer, int F_num) { //Name buffer에 저장된 데이터에서 필요한 정보를 추출해 name정보를 받아오는 함수 
+	char tmp[3]; // 3개의 저장된 정보를 받아오는 배열
+	int index = 0;
+	for (int i = 0; buffer[i] != 0; i = i + 3) {
+		tmp[0] = buffer[i];
+		tmp[1] = buffer[i + 1];
+		tmp[2] = buffer[i + 2]; // tmp 안에 저장된 buffer 정보를 받은후
+		FRIEND[F_num].name[index] = CheckUnchar(tmp); // Checkunchar 로 3개중 검증된 하나의 정보를 추출한다.
+		index++;
+	}
+	//이 과정을 buffer 배열이 끝날때 까지 반복해 한글자씩 name구조체 변수에 받아온다.
+}
+
+void FRIEND_READ() { 
+	// 인코더 파일에서 각각의 정보들을 해당하는 buffer에 저장한다. but, 동맹수 ID길이 이름길이의 경우에는 ReadUnchar를 이용해 바로 필요한 정보를 읽어내어 저장한다. 
+	num = ReadUnchar(); // 동맹수를 읽어 저장한다.
+	for (int i = 0; i < num; i++) {
+		FRIEND[i].I_length = ReadUnchar(); // ID길이 저장
+		fread(FRIEND[i].buffer, sizeof(char), FRIEND[i].I_length * 3, fp1); // ID buffer에 정보입력
+		FRIEND[i].N_length = ReadUnchar(); // name길이 
+		fread(FRIEND[i].buffer_n, sizeof(char), FRIEND[i].N_length * 3, fp1); //name buffer에 정보입력
+		fread(FRIEND[i].buffer_g, sizeof(char), 3, fp1); // 성별 buffer에 정보입력
+		fread(FRIEND[i].buffer_age, sizeof(char), 3, fp1); // 나이 buffer에 정보입력
+	}
+}
+void FRIEND_SAVE() { // buffer에 있는 정보를 걸러서 해당하는 구조체변수에 저장
 	for (int i = 0; i < 3; i++) {
 		Checkstr_I(FRIEND[i].buffer, i);
 		Checkstr_N(FRIEND[i].buffer_n, i);
@@ -220,7 +222,7 @@ void FRIEND_SAVE() { // buffer에 있는 정보를 걸러서 출력하고자하�
 	}
 }
 
-void fprint() {
+void fprint() { // 구조체변수안에 있는 데이터를 형식에 맞춰서 fprintf로 텍스트파일에 입력해준다.
 	fprintf(fp2, "*FRIENDS LIST*\n");
 	for (int i = 0; i < num; i++) {
 		fprintf(fp2, "FRIEND%d ID: %s\n", i + 1, FRIEND[i].ID);
